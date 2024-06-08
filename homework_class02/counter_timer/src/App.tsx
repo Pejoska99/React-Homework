bdaa92abc7867fabf3931c795e7e4d1aae1e7053
Pejoska99 <biljanapejoska985@gmail.com>
2024-06-08 20:30:55 +0200

    add commit

--- homework_class02/counter_timer/src/App.tsx
+++ homework_class02/counter_timer/src/App.tsx
@@ -3,15 +3,11 @@ import CounterBy from './components/CounterBy'
 import Timer from './components/Timer'
 
 function App() {
-  
-
   return (
-    
-    <div className="App">
+    <>
       <CounterBy />
       <Timer />
-    </div>
-
+    </>
   )
 
 }
